318c57019f6c2480f87f49d6e194ac98bc8287e7
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2023-08-13 22:07:53 +0300

    Solved: Composition Challenge

---
+++ src/SmartKitchen.java
@@ -0,0 +1,32 @@
+public class SmartKitchen {
+
+    private CoffeeMaker brewMaster;
+    private DishWasher dishWasher;
+    private Refrigerator iceBox;
+
+    public SmartKitchen(CoffeeMaker brewMaster, DishWasher dishWasher, Refrigerator iceBox) {
+        this.brewMaster = brewMaster;
+        this.dishWasher = dishWasher;
+        this.iceBox = iceBox;
+    }
+
+    public void addWater(){
+        brewMaster.setHasWorkToDo(true);
+    }
+
+    public void pourMilk(){
+        iceBox.setHasWorkToDo(true);
+    }
+
+    public void loadDishWasher(){
+        dishWasher.setHasWorkToDo(true);
+    }
+
+    public void setKitchenState() {
+
+    }
+
+    public void doKitchenWork(){
+
+    }
+}
