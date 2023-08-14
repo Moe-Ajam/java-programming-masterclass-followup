3f1838c1a464b4bd2bd685eb9a5fbb6882019b59
Moe Ajam <mahmoud.othman.ajam@gmail.com>
2023-08-14 21:15:59 +0300

    Solved the prime challenge

--- src/LargestPrime.java
+++ src/LargestPrime.java
@@ -3,14 +3,14 @@ public class LargestPrime {
         System.out.println(getLargestPrime(7));
     }
 
-    public static int getLargestPrime(int number){
-        if(number <= 1) return -1;
+    public static int getLargestPrime(int number) {
+        if (number <= 1) return -1;
 
         int currentPrime = number;
-        for (int i=2; i<=number;i++){
-            for(int j=2; j<=number;j++){
-                if(i * j == number)
-                    if(i > j) {
+        for (int i = 2; i <= number; i++) {
+            for (int j = 2; j <= number; j++) {
+                if (i * j == number)
+                    if (i > j) {
                         currentPrime = i;
                     } else {
                         currentPrime = j;
